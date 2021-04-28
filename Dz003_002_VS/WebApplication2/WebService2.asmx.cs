ce6fd79c5923e8b0c34ca80ab1ec084ddc523449
ivona99 <57632353+ivona99@users.noreply.github.com>
2021-04-28 14:27:28 +0200

    Add files via upload

---
+++ Dz003_002_VS/WebApplication2/WebService2.asmx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace WebApplication2
+{
+    /// <summary>
+    /// Summary description for WebService2
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService2 : System.Web.Services.WebService
+    {
+
+        [System.Web.Services.WebMethod]
+        public DataTable getEmployeerByID(string Number)
+        {
+            string connString = "SERVER=localhost" + ";" +
+                "DATABASE=sakila;" +
+                "UID=root;" +
+                "PASSWORD=;";
+
+            MySqlConnection cnMySQL = new MySqlConnection(connString);
+
+            MySqlCommand cmdMySQL = cnMySQL.CreateCommand();
+
+            MySqlDataReader reader;
+
+            string querry = "select * from customer where customer_id =" + Number;
+
+            cmdMySQL.CommandText = querry;
+            cnMySQL.Open();
+
+            reader = cmdMySQL.ExecuteReader();
+
+            DataTable dt = new DataTable();
+            dt.Load(reader);
+
+
+            cnMySQL.Close();
+
+            return dt;
+        }
+    }
+}
